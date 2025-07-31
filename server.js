56dbfbbc2b4dd99d138fb5a5b04805b01bbd8976
Shivam Tanwar <shivamtanwar0801@gmail.com>
2025-07-31 16:56:29 +0530

    Update CORS settings

--- server.js
+++ server.js
@@ -11,16 +11,19 @@ const { socketService } = require("./services/socketService");
 
 const app = express();
 const server = http.createServer(app);
+
+const allowedOrigin = "https://crypto-crash-frontend-ochre.vercel.app/"
+
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: allowedOrigin,
     methods: ["GET", "POST"],
   },
 });
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: allowedOrigin,
     methods: ["GET", "POST"],
   })
 );
